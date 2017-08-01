8eb5efc37d32ce83c0dd4c39d40c38346de63612
David Carboni <david.carboni@ros.gov.uk>
2017-08-01 17:31:01 +0100

    Simplified demo app.

--- demo.py
+++ demo.py
@@ -7,21 +7,13 @@ NB the example Spring Boot lines come from a file and are re-logged through the 
 timestamps, process IDs and thread names will (correctly) differ.
 
 """
-from log import log, regex
-import os
+from regex import regex
 import logging
+import logging_standard
 from flask import Flask, g
 from flaskb3 import b3
 
-# Logging
-
-debug = bool(1)
-# os.getenv("FLASK_DEBUG"))
-level = logging.DEBUG if debug else logging.WARNING
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-# logger.warning("Level=" + logging.getLevelName(level))
-# logger.warning("Level=" + logging.getLevelName(logger.level))
+app = Flask("myapp")
 
 
 def parsing_demo():
@@ -42,23 +34,22 @@ def parsing_demo():
 
             if values.get("transaction"):
                 tracing_info = values["transaction"]
-                log._app_name = tracing_info["app"]
+                logging_standard._app_name = tracing_info["app"]
                 setattr(g, b3.b3_trace_id, tracing_info["id"])
                 setattr(g, b3.b3_span_id, tracing_info["span"])
                 if tracing_info["exported"] == "true":
                     setattr(g, b3.b3_sampled, "1")
 
-            extra = log.extra(logger)
             level = values.get("log_level")
             message = values["log_message"]
             if level == "ERROR":
-                logger.error(message, extra=extra)
+                logger.error(message)
             elif level == "WARN":
-                logger.warning(message, extra=extra)
+                logger.warning(message)
             elif level == "INFO":
-                logger.info(message, extra=extra)
+                logger.info(message)
             elif level == "DEBUG":
-                logger.debug(message, extra=extra)
+                logger.debug(message)
 
             print("---")
 
@@ -76,23 +67,16 @@ def logging_demo():
 
 
 if __name__ == '__main__':
-    app = Flask("myapp")
-    log.init(app)
+
+    logging_standard.init(app)
     with app.app_context():
         logging_demo()
         # parsing_demo()
-
-    # Port
-
-    port = os.getenv("PORT", "5000")
-    logger.info("PORT is " + str(port))
-    logger.info("FLASK_DEBUG is " + str(debug))
 
     # Go!
 
     app.run(
         host="0.0.0.0",
-        port=int(port),
-        debug=debug,
+        debug=True,
         threaded=True
     )
